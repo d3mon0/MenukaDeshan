62ae0b2eec840db741e81f26c7e93d0356440397
cypenet <dextermd0@gmail.com>
2022-01-27 18:37:15 +0530

    First site

---
+++ js/darkmod.js
@@ -0,0 +1,19 @@
+var icon = document.getElementById("icon");
+var head = document.getElementById("head");
+var logo = document.getElementById("logo");
+var back = document.getElementById("back");
+
+    icon.onclick = function(){
+        document.body.classList.toggle("darkmod");
+        if(document.body.classList.contains("darkmod")){
+            icon.src = "img/sun.png";
+            head.src = "img/logo3.png"
+            logo.src = "img/png.png"
+            back.src = "img/pred.png"
+        }else{
+            icon.src = "img/moon.png";
+            head.src = "img/logoDARK.png"
+            logo.src = "img/logoDARK.png"
+            back.src = "img/pattern.png"
+            }
+    }
